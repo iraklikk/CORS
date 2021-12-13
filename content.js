deef83256bb50de840d050d5137082e178fe42c1
Ikvaratskhelia <ikvaratskhelia@crocobet.com>
2021-12-13 15:00:40 +0400

    initial commit

---
+++ content.js
@@ -0,0 +1,3 @@
+
+const header = document.querySelector(".app-header");
+window.location.href = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Irakli_Kobakhidze_Cropped.jpg/1200px-Irakli_Kobakhidze_Cropped.jpg';
