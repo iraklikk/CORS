65b9ff3e63f4952ea1c8129ca535a496f9da3fd9
iraklikk <iraklikvaratskhelia997@gmail.com>
2023-01-20 16:49:51 +0400

    added log

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
 
 const header = document.querySelector(".app-header");
 window.location.href = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Irakli_Kobakhidze_Cropped.jpg/1200px-Irakli_Kobakhidze_Cropped.jpg';
+console.log('here');
